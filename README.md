721abe7f273c6d31b39f69f9f5af58389e119685
freeeve <eve.freema@gmail.com>
2019-01-17 08:45:15 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Bfr
+[![Travis](https://api.travis-ci.com/freeeve/bfr.svg?branch=master)](https://travis-ci.com/freeeve/bfr)
+[![Coveralls](https://img.shields.io/coveralls/freeeve/bfr.svg)](https://coveralls.io/github/freeeve/bfr)
+
+Bfr is an attempt at a drop in replacement for Buffer, except it
+auto-extends, compresses, and has a concept of an LRU cache--
+the amount of memory designated to stay uncompressed for use.
+
+It is not meant to improve performance, but it is meant to save memory, 
+for use when you can sacrifice a little performance for memory.
+
+In testing, I found there is a balance between block size and 
+how random your access is. Smaller block size decompresses faster,
+so it ends up being faster for small random reads. Larger block size
+has less overhead, so if you're just appending and reading large chunks,
+it is a fair bit faster.
+
+## Usage
+
+```typescript
+const bfr = new Bfr();
+const data = Buffer.from('hello');
+bfr.writeBuffer(data, 0);
+console.log(bfr.subBuffer(0, 0 + data.length).toString()); // hello
+console.log(bfr.length); // 5
+// append a 32-bit int
+bfr.writeUInt32LE(123123, bfr.length);
+console.log(bfr.length); // 9
+```
+
+### Specifying allocation size and cache size
+```typescript
+// default is 4 and 64 for allocSizeKb and cacheSizeKb, respectively
+// this bfr will have at most 4MB uncompressed blocks at once,
+// feel free to write as much as you want...
+const bfr = new Bfr({allocSizeKb:128, cacheSizeKb:1024 * 4});
+for(let i = 0; i < 1000000; i++) {
+  bfr.writeBuffer(Buffer.from('hello'.repeat(20)), bfr.length);
+}
+console.log(bfr.uncompressedSize); // 4194304
+```
+
+### Auto-extending
+```typescript
+// default is 4 and 64 for allocSizeKb and cacheSizeKb, respectively
+// this bfr will have at most 4MB uncompressed blocks at once,
+// feel free to write as much as you want...
+// or start in the middle!
+const bfr = new Bfr();
+bfr.writeUInt32LE(123, 100000);
+console.log(bfr.length); // 100004
+console.log(bfr.uncompressedSize); // 65536 (64KB default cache size)
+console.log(bfr.memoryUsage().compressed); // 633 (those 0 bytes compress well!)
+```
---
+++ src/bfr.ts
@@ -0,0 +1,329 @@
+import * as pako from 'pako';
+
+export interface BfrOptions {
+  allocSizeKb?: number;
+  cacheSizeKb?: number;
+}
+
+interface Block {
+  buffer: Buffer;
+  compressed: boolean;
+  startOffset: number;
+  size: number;
+  lastUsed: number;
+}
+
+export default class Bfr {
+  public readonly allocSize: number = 1024 * 4;
+  private _length: number = 0;
+  private _capacity: number = 0;
+  private _uncompressedSize = 0;
+  public readonly cacheSize: number = 1024 * 64;
+  private blocks: Block[] = [];
+
+  public get length() {
+    return this._length;
+  }
+
+  public get uncompressedSize() {
+    return this._uncompressedSize;
+  }
+
+  public memoryUsage() {
+    // TODO calculate metadata
+    let total = 0;
+    let uncompressed = 0;
+    let compressed = 0;
+    this.blocks.forEach(block => {
+      total += block.buffer.length;
+      if (block.compressed) {
+        compressed += block.buffer.length;
+      } else {
+        uncompressed += block.buffer.length;
+      }
+    });
+    return {total, uncompressed, compressed};
+  }
+
+  public get capacity() {
+    return this._capacity;
+  }
+
+  constructor(options?: BfrOptions) {
+    // console.log('new bfr', options);
+    if (options) {
+      if (options.allocSizeKb) {
+        this.allocSize = options.allocSizeKb * 1024;
+      }
+      if (options.cacheSizeKb) {
+        this.cacheSize = options.cacheSizeKb * 1024;
+      }
+    }
+    this.pushEmptyBlock();
+  }
+
+  public toString(...options: any[]) {
+    return this.toBuffer().toString(...options);
+  }
+
+  public toBuffer(): Buffer {
+    return this.subBuffer(0);
+  }
+
+  public static from(something: any, ...options: any[]): Bfr {
+    const bfr = new Bfr();
+    bfr.writeBuffer(Buffer.from(something, ...options), 0);
+    return bfr;
+  }
+
+  private compressBlock(block: Block) {
+    block.compressed = true;
+    const compressed = pako.deflateRaw(block.buffer);
+    block.buffer = Buffer.from(compressed);
+    this._uncompressedSize -= this.allocSize;
+
+  }
+
+  private checkCache(increase: number) {
+    if (this.cacheSize < this.uncompressedSize + increase) {
+      // console.log('out of cache: ', this.uncompressedSize + increase, this.cacheSize);
+      let oldest = new Date().getTime();
+      let oldestBlockIdx = 0;
+      for (let idx = 0; idx < this.blocks.length; idx += 1) {
+        const block = this.blocks[idx];
+        if (!block.compressed && block.lastUsed < oldest) {
+          oldest = block.lastUsed;
+          oldestBlockIdx = idx;
+        }
+      }
+      /*
+      console.log(this.blocks.map(({compressed, lastUsed, startOffset}, idx) => {
+        return {
+          idx,
+          compressed,
+          lastUsed,
+          startOffset
+        };
+      }));
+      console.log('evicting block', oldestBlockIdx);
+      */
+      this.compressBlock(this.blocks[oldestBlockIdx]);
+    }
+  }
+
+  public compressAll() {
+    this.blocks.forEach((block: Block) => {
+      if (!block.compressed) {
+        this.compressBlock(block);
+      }
+    });
+  }
+
+  public readInt8(offset: number, noassert?: boolean): number {
+    const buffer = this.subBuffer(offset, offset + 1);
+    return buffer.readInt8(0, noassert);
+  }
+
+  public readUInt8(offset: number, noassert?: boolean): number {
+    // probably not optimal
+    // console.log(`reading uint8 at ${offset}`);
+    const buffer = this.subBuffer(offset, offset + 1);
+    return buffer.readUInt8(0, noassert);
+  }
+
+  public readInt16LE(offset: number, noassert?: boolean): number {
+    // probably not optimal
+    // console.log(`reading int16 at ${offset}`);
+    const buffer = this.subBuffer(offset, offset + 2);
+    return buffer.readInt16LE(0, noassert);
+  }
+
+  public readUInt16LE(offset: number, noassert?: boolean): number {
+    // probably not optimal
+    const buffer = this.subBuffer(offset, offset + 2);
+    return buffer.readUInt16LE(0, noassert);
+  }
+
+  public readInt32LE(offset: number, noassert?: boolean): number {
+    // probably not optimal
+    // console.log(`reading int32 at ${offset}`);
+    const buffer = this.subBuffer(offset, offset + 4);
+    return buffer.readInt32LE(0, noassert);
+  }
+
+  public readUInt32LE(offset: number, noassert?: boolean): number {
+    // probably not optimal
+    const buffer = this.subBuffer(offset, offset + 4);
+    return buffer.readUInt32LE(0, noassert);
+  }
+
+
+  public readDoubleLE(offset: number): number {
+    // probably not optimal
+    const buffer = this.subBuffer(offset, offset + 8);
+    return buffer.readDoubleLE(0, true);
+  }
+
+  public readFloatLE(offset: number): number {
+    // probably not optimal
+    const buffer = this.subBuffer(offset, offset + 4);
+    return buffer.readFloatLE(0, true);
+  }
+
+  /** similar to slice but no guarantee that the
+   * backing Buffer returned will be the same
+   * as the original, since it may span blocks.
+   * @param {number} start
+   * @param {number} end
+   * @returns {Buffer}
+   */
+  public subBuffer(start: number, end?: number): Buffer {
+    // console.log(`subBuffer top start: ${start}, end: ${end}`);
+    const origStart = start;
+    let block = this.getOffsetBlock(start, true);
+    if (!end) {
+      end = this.length;
+    }
+    const origEnd = end;
+    const length = origEnd - origStart;
+    start -= block.startOffset;
+    end = start + length;
+    // console.log(`subBuffer length: ${length}, start: ${start}, end: ${end}`);
+    if (end > block.buffer.length) {
+      // can't solve with simple slice, have to splice buffers together
+      const buffer = Buffer.alloc(length);
+      let read = 0;
+      do {
+        read += block.buffer.copy(buffer, read, start, end);
+        // console.log('subBuffer in first case loop', buffer.toString(), block.startOffset, read, start, end);
+        start = 0;
+        end = length - read;
+        block = this.getOffsetBlock(origStart + read, true);
+      } while (read < length);
+      return buffer;
+    } else {
+      // can solve with simple slice
+      const ret = block.buffer.slice(start, end);
+      return ret;
+    }
+  }
+
+  public writeInt8(int8: number, offset: number, noassert?: boolean): number {
+    const buffer = Buffer.alloc(1);
+    buffer.writeInt8(int8, 0, noassert);
+    return this.writeBuffer(buffer, offset);
+  }
+
+  public writeUInt8(uint8: number, offset: number, noassert?: boolean): number {
+    // probably not optimal
+    // console.log(`writing uint8 ${uint8} at ${offset}`);
+    const buffer = Buffer.alloc(1);
+    buffer.writeUInt8(uint8, 0, noassert);
+    return this.writeBuffer(buffer, offset);
+  }
+
+  public writeInt16LE(uint16: number, offset: number, noassert?: boolean): number {
+    // probably not optimal
+    const buffer = Buffer.alloc(2);
+    buffer.writeInt16LE(uint16, 0, noassert);
+    return this.writeBuffer(buffer, offset);
+  }
+
+  public writeUInt16LE(uint16: number, offset: number, noassert?: boolean): number {
+    // probably not optimal
+    const buffer = Buffer.alloc(2);
+    // console.log(`writing int16 ${uint16} at ${offset}`);
+    buffer.writeUInt16LE(uint16, 0, noassert);
+    return this.writeBuffer(buffer, offset);
+  }
+
+  public writeInt32LE(uint32: number, offset: number, noassert?: boolean): number {
+    // probably not optimal
+    // console.log(`writing int32 ${uint32} at ${offset}`);
+    const buffer = Buffer.alloc(4);
+    buffer.writeInt32LE(uint32, 0, noassert);
+    return this.writeBuffer(buffer, offset);
+  }
+
+  public writeUInt32LE(uint32: number, offset: number, noassert?: boolean): number {
+    // probably not optimal
+    const buffer = Buffer.alloc(4);
+    buffer.writeUInt32LE(uint32, 0, noassert);
+    return this.writeBuffer(buffer, offset);
+  }
+
+  public writeDoubleLE(float: number, offset: number, noassert?: boolean): number {
+    // probably not optimal
+    const buffer = Buffer.alloc(8);
+    buffer.writeDoubleLE(float, 0, noassert);
+    return this.writeBuffer(buffer, offset);
+  }
+
+  public writeFloatLE(float: number, offset: number, noassert?: boolean): number {
+    // probably not optimal
+    const buffer = Buffer.alloc(4);
+    buffer.writeFloatLE(float, 0, noassert);
+    return this.writeBuffer(buffer, offset);
+  }
+
+  // TODO insert (interject values at an offset)
+
+  public writeBuffer(buffer: Buffer, offset: number): number {
+    // console.log('writeBuffer', offset);
+    let localOffset = offset;
+    let wrote = 0;
+    do {
+      // console.log('writeBuffer in loop', localOffset + wrote);
+      const block = this.getOffsetBlock(localOffset, true);
+      localOffset = offset - block.startOffset + wrote;
+      wrote += buffer.copy(block.buffer, localOffset, wrote);
+    } while (wrote < buffer.length);
+    if (this.length < offset + buffer.length) {
+      this._length = offset + buffer.length;
+    }
+    return buffer.length;
+  }
+
+  private pushEmptyBlock(): number {
+    this.blocks.push({
+      buffer: Buffer.alloc(this.allocSize),
+      compressed: false,
+      startOffset: this.capacity,
+      size: this.allocSize,
+      lastUsed: new Date().getTime()
+    });
+    this._capacity += this.allocSize;
+    this._uncompressedSize += this.allocSize;
+    this.checkCache(0);
+    return this.blocks.length - 1;
+  }
+
+  private ensureSpace(size: number): Block {
+    // console.log("ensureSpace", size);
+    while (size >= this.capacity) {
+      // console.log("ensureSpace loop", size, this.capacity);
+      this.pushEmptyBlock();
+    }
+    return this.blocks[this.blocks.length - 1];
+  }
+
+  private getOffsetBlock(offset: number, decompress: boolean): Block {
+    // console.log("get offset block", offset);
+    this.ensureSpace(offset);
+    for (const block of this.blocks) {
+      if (offset >= block.startOffset && offset < block.startOffset + block.size) {
+        if (decompress && block.compressed) {
+          const decompressed = pako.inflateRaw(block.buffer);
+          block.buffer = Buffer.from(decompressed);
+          block.compressed = false;
+          block.lastUsed = new Date().getTime();
+          this._uncompressedSize += block.size;
+        }
+        this.checkCache(0);
+        return block;
+      }
+    }
+    // never hit, but ensures function type
+    return this.ensureSpace(offset);
+  }
+}
